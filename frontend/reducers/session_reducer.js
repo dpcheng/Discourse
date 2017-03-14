888d86d62524634bb4155e8a524e4a66ee85f5f5
Dylan <dylan.patrick.cheng@gmail.com>
2017-03-14 12:07:50 -0700

    write SessionReducer. may change _defaultState and State in the future

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,23 @@
+import { RECEIVE_CURRENT_USER, RECEIVE_ERRORS } from '../actions/session_actions';
+import merge from 'lodash/merge';
+
+const _defaultState = {
+  currentUser: null,
+  errors: []
+};
+
+const SessionReducer = (state = _defaultState, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      const currentUser = action.currentUser;
+      return merge({}, _defaultState, { currentUser });
+    case RECEIVE_ERRORS:
+      const errors = action.errors;
+      return merge({}, state, { errors });
+    default:
+      return state;
+  }
+};
+
+export default SessionReducer;
