d16fe3189ae7a0670b6361b68dbed36a4663d04f
Dylan <dylan.patrick.cheng@gmail.com>
2017-03-14 14:57:18 -0700

    finish writing session_form. still need to test

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -29,28 +29,38 @@ class SessionForm extends React.Component {
     let buttonText = "Continue";
     let linkMessage = "Already have an account? ";
     let linkText = "Login";
+    let linkPath = "/register";
 
     if (this.props.formType === 'login') {
       header = "WELCOME BACK.";
       buttonText = "Login";
       linkMessage = "Need an account? ";
       linkText = "Register";
+      linkPath = "/login";
     }
 
     const {loggedIn, errors, processForm, formType} = this.props;
 
     return (
-      <form className="session-form">
+      <form className="session-form" onSubmit={ this.handleSubmit }>
         <h1 className="form-header">{header}</h1>
+
         <ul className="form-errors">
           {errors.map((error, idx) => (<li key={idx} >{error}</li>))}
         </ul>
+
         <label className="form-label">USERNAME</label>
         <input type="text" className="form-field"
           onChange={this.updateField("username")}>{this.state.username}</input>
+
         <label className="form-label">PASSWORD</label>
         <input type="password" className="form-field"
           onChange={this.updateField("password")}>{this.state.password}</input>
+
+        <input type="submit" className="form-button" value={buttonText} />
+        <span>{linkMessage}
+          <Link to={linkPath} className="form-link" >{linkText}</Link>
+        </span>
       </form>
     );
   }
