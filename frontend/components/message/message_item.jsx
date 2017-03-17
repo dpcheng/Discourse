0fea5c01ffc7756f27d20811abaac4e411604564
Dylan <dylan.patrick.cheng@gmail.com>
2017-03-16 17:08:03 -0700

    fix bug where long paragraphs will change avatar shape

--- frontend/components/message/message_item.jsx
+++ frontend/components/message/message_item.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 const MessageItem = ({ message }) => {
   return (
     <ul className="message-item" key={message.id}>
-      <div className="message-avatar" key={`${message.id}-avatar`}
-        >{message.username[0]}</div>
+      <div className="message-avatar-col" >
+        <div className="message-avatar" key={`${message.id}-avatar`}
+          >{message.username[0]}</div>
+      </div>
       <div className="message-item-contents" >
         <li className="message-username"
           key={`${message.id}-username`}>{message.username} </li>
