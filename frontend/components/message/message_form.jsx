3087260e672538de924a4fff616ea971993765a5
Dylan <dylan.patrick.cheng@gmail.com>
2017-03-22 17:44:28 -0700

    add subChannel name to message form

--- frontend/components/message/message_form.jsx
+++ frontend/components/message/message_form.jsx
@@ -26,19 +26,27 @@ class MessageForm extends React.Component {
 
   render() {
     const { subChannelId, subChannels, currentUser } = this.props;
-
+    let subId = parseInt(subChannelId);
     let subChannelName = "";
     let disabled = "disabled";
-    if (currentUser.direct_messages.includes(parseInt(subChannelId))) {
+    if (currentUser.direct_messages.includes(subId)) {
       let users = [];
       this.props.users.forEach(user => {
-        if (user.direct_messages.includes(parseInt(subChannelId))) {
+        if (user.direct_messages.includes(subId)) {
           users.push(user.username);
         }
       });
 
       const list = users.join(", ");
-      subChannelName = `Private Chat with ${list}`;
+      let chatroomName = "Private Chat";
+      if (subChannels[subId]) {
+        chatroomName = subChannels[subId].name;
+      }
+
+      subChannelName = `${chatroomName} with ${list}`;
+      if (users.length === 1) {
+        subChannelName = `Add a user to ${subChannels[subId].name}! Click the +`;
+      }
       disabled = false;
     } else if (subChannels[subChannelId]) {
       subChannelName = `Message ${subChannels[subChannelId].name}`;
