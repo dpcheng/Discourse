e19e80c5773f3214f63356d3d9c379a4cf118d99
Dylan <dylan.patrick.cheng@gmail.com>
2017-05-29 21:13:11 -0700

    change MessageItem to be a component

--- frontend/components/message/message_item.jsx
+++ frontend/components/message/message_item.jsx
@@ -1,57 +1,71 @@
 import React from 'react';
 
-const MessageItem = ({ message }) => {
-  let avatarColor;
-
-  switch (message.user_id % 10) {
-    case 1:
-      avatarColor = "first";
-      break;
-    case 2:
-      avatarColor = "second";
-      break;
-    case 3:
-      avatarColor = "third";
-      break;
-    case 4:
-      avatarColor = "fourth";
-      break;
-    case 5:
-      avatarColor = "fifth";
-      break;
-    case 6:
-      avatarColor = "sixth";
-      break;
-    case 7:
-      avatarColor = "seventh";
-      break;
-    case 8:
-      avatarColor = "eigth";
-      break;
-    case 9:
-      avatarColor = "ninth";
-      break;
-    default:
-      avatarColor = "tenth";
+class MessageItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.showImage = this.showImage.bind(this);
   }
 
+  showImage() {
+    const image_url = this.props.message.image_url;
+    if (image_url) {
+      return <img className="message-image" src={image_url} />;
+    }
+  }
 
-  return (
-    <ul className="message-item" key={message.id}>
-      <div className="message-avatar-col" >
-        <div className={`message-avatar ${avatarColor}`}
-          key={`${message.id}-avatar`} >{message.username[0]}</div>
-      </div>
-      <div className="message-item-contents" >
-        <li className={`message-username ${avatarColor}`}
-          key={`${message.id}-username`}>{message.username}<div className="message-timestamp">{message.created_at}</div>
+  render() {
+    let message = this.props.message;
+    let avatarColor;
+
+    switch (message.user_id % 10) {
+      case 1:
+      avatarColor = "first";
+      break;
+      case 2:
+      avatarColor = "second";
+      break;
+      case 3:
+      avatarColor = "third";
+      break;
+      case 4:
+      avatarColor = "fourth";
+      break;
+      case 5:
+      avatarColor = "fifth";
+      break;
+      case 6:
+      avatarColor = "sixth";
+      break;
+      case 7:
+      avatarColor = "seventh";
+      break;
+      case 8:
+      avatarColor = "eigth";
+      break;
+      case 9:
+      avatarColor = "ninth";
+      break;
+      default:
+      avatarColor = "tenth";
+    }
+
+
+    return (
+      <ul className="message-item" key={message.id}>
+        <div className="message-avatar-col" >
+          <div className={`message-avatar ${avatarColor}`}
+            key={`${message.id}-avatar`} >{message.username[0]}</div>
+        </div>
+        <div className="message-item-contents" >
+          <li className={`message-username ${avatarColor}`}
+            key={`${message.id}-username`}>{message.username}<div className="message-timestamp">{message.created_at}</div>
         </li>
-        <img className="message-image" src="" />
         <li className="message-text"
           key={`${message.id}-text`}>{message.text}</li>
       </div>
     </ul>
-  );
-};
+    );
+  }
+}
 
 export default MessageItem;
