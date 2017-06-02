580de78d35c44c63c8a36f3f1ae015386aa1ebb8
Dylan <dylan.patrick.cheng@gmail.com>
2017-06-01 17:29:18 -0700

    fix new channel modal style

--- frontend/components/channel/channel_list.jsx
+++ frontend/components/channel/channel_list.jsx
@@ -65,6 +65,7 @@ class ChannelList extends React.Component {
         left                  : '50%',
         right                 : 'auto',
         bottom                : 'auto',
+        height                : '170px',
         marginRight           : '-50%',
         transform             : 'translate(-50%, -50%)',
         backgroundColor       : 'rgba(228, 228, 228, 1.0)'
